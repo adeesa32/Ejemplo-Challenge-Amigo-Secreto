dc717daa626cde72f055bf0d8b7aaf91cd4490e7
Adelina Escobar Acevedo <57870687+adeesa32@users.noreply.github.com>
2025-01-30 09:20:13 -0600

    Sube archivos base del proyecto

---
+++ app.js
@@ -0,0 +1,7 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//autor del código: Adelina Escobar Acevedo
+//Fecha de creación: 29 de enero de 2025
+// objetivo: 1.-Solicitar al usuario nombres de personas que se agregan al presionar el botón agregar
+//2.- al presionar el botón amigo secreto, se elige una persona
+// *Restricciones: Si la cadena es vacía se pide un nombre válido
+
